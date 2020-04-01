6eecbbfe838691c4ff35a4e01e37ca2eca2765c6
Felipe Cândido <felipe.candido8@gmail.com>
2020-04-01 19:04:26 -0300

    Front-end do App

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express'); // simples assim o node entende que é um pacote
-const express = require('cors'); // segurança NPM INSTALL CORS
+const cors = require('cors'); // segurança NPM INSTALL CORS
 const routes = require('./routes'); // com ./ o node entende que é arquivo
 
 // para voltar uma pasta usa-se ../, quando unsa ./ sozinho ele referencia a pasta atual
@@ -11,7 +11,7 @@ const app = express();
 /**
  * Informa que todas as respostas serão em json
  */
-app.use(cors())
+app.use(cors());
 app.use(express.json()); 
 
 app.use(routes);
